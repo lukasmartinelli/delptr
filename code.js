2252a2461f97abc7a231e4d3fa60b937a9c1e85d
Lukas Martinelli <me@lukasmartinelli.ch>
2014-12-20 16:01:18 +0100

    Show fragment in block

--- code.js
+++ code.js
@@ -9,12 +9,43 @@ module.exports = function (codeMargin) {
             to: to > lastLine ? lastLine : to
         };
     };
+    var findSurroundingBlock = function (linenumber, lines) {
+        var findOpenBracket = function() {
+            for(var i = linenumber - 1; i > 0; i--) {
+                if(lines[i].indexOf('{') > -1) {
+                    if(lines[i].trim() === '{') {
+                        return i;
+                    } else {
+                        return i + 1;
+                    }
+                }
+            }
+            return -1;
+        };
+        var findClosedBracket = function () {
+            for(var i = linenumber - 1; i < lines.length; i++) {
+                if(lines[i].indexOf('}') > -1) {
+                    return i + 1;
+                }
+            }
+            return -1;
+        };
+        return {
+            from: findOpenBracket(),
+            to: findClosedBracket()
+        };
+    };
     return {
         fragment: function (lineNumber, file) {
             var lines = file.split('\n');
-            var range = maxFragmentRange(lineNumber, lines.length);
-            var fragment = lines.slice(range.from - 1, range.to).join('\n');
-            return fragment;
+            var block = findSurroundingBlock(lineNumber, lines);
+
+            if(block.from > -1 && block.to > -1) {
+                return lines.slice(block.from - 1, block.to).join('\n');
+            } else {
+                var range = maxFragmentRange(lineNumber, lines.length);
+                return lines.slice(range.from - 1, range.to).join('\n');
+            }
         }
     };
 };
--- tests/code.js
+++ tests/code.js
@@ -9,27 +9,39 @@ var sampleFile = '#include <iostream>\n' +
                  '    return 0;\n' +
                  '}';
 
+var newlineSampleFile = '#include <iostream>\n' +
+                 'using namespace std;\n' +
+                 'int main()\n' +
+                 '{\n' +
+                 '    cout << "Hello World!" << endl;\n' +
+                 '    return 0;\n' +
+                 '}';
+
 describe('fragment', function() {
-    it('returns line plus margin below and above', function() {
-        var fragment = code.fragment(4, sampleFile);
-        should(fragment).equal(
-            'int main() {\n' +
-            '    cout << "Hello World!" << endl;\n' +
-            '    return 0;'
-        );
-    });
-    it('returns line and margin above if line is at end of file', function() {
-        var fragment = code.fragment(6, sampleFile);
-        should(fragment).equal(
-             '    return 0;\n' +
-             '}'
-        );
-    });
     it('returns line and margin below if line is at start of file', function() {
         var fragment = code.fragment(1, sampleFile);
         should(fragment).equal(
             '#include <iostream>\n' +
             'using namespace std;'
+        );
+    });
+    it('returns block between opening and closing bracket', function() {
+        var fragment = code.fragment(4, sampleFile);
+        should(fragment).eql(
+             'int main() {\n' +
+             '    cout << "Hello World!" << endl;\n' +
+             '    return 0;\n' +
+             '}'
+        );
+    });
+    it('can deal with newline brackets', function() {
+        var fragment = code.fragment(4, newlineSampleFile);
+        should(fragment).eql(
+             'int main()\n' +
+             '{\n' +
+             '    cout << "Hello World!" << endl;\n' +
+             '    return 0;\n' +
+             '}'
         );
     });
 });
