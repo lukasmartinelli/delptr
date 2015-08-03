db305ed658a3065b55d5b328eadd7783105d4b7f
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-03 21:10:04 +0200

    Hope i correctly nested server and client now

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 /*eslint new-cap:0 */
 'use strict';
-var path = require('path');
+
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var path = require('path');
 var linter = require('./linter');
 var log = require('./log');
 
@@ -18,78 +21,81 @@ if(!options.accessToken) {
     throw 'No Github Access token specified.';
 }
 
-console.log(options);
-
 var github = require('./github')(options.accessToken);
 var code = require('./code')(options.codeMargin);
 
-var serverSocket = require('socket.io')(http);
-serverSocket.set('origins', '*:*');
-var clientSocket = require('socket.io-client')(options.url);
+var ioClient = require('socket.io-client')(options.url);
 var lastError;
 
-clientSocket.on('connect_error', function(err) {
-    console.error(err);
+server.listen(options.port);
+app.use(express.static(path.join(__dirname, '/public')));
+
+io.on('connection', function(socket) {
+    if(lastError) {
+        socket.emit('linterror', lastError);
+    }
+
+    function handlePushEvent(event) {
+        function checkRepo(callback) {
+            event.payload.commits.forEach(function (commit) {
+                github.patch(event.repo, commit, function (patch) {
+                    callback(commit, linter.checkPatch(patch));
+                });
+            });
+        };
+
+        function handleError(commit, filename, error) {
+            github.file(event.repo.name, commit.sha, filename, function(file) {
+                lastError = {
+                    actor: event.actor,
+                    repo: event.repo,
+                    commit: commit,
+                    error: error,
+                    filename: filename,
+                    code: code.fragment(error.linenumber, file)
+                };
+                socket.emit('linterror', lastError);
+            });
+        };
+
+        function handleCheck(commit, lintResults) {
+            if (lintResults.length === 0) {
+                log.skip(event.repo.name, commit.sha);
+            }
+
+            lintResults.forEach(function processLintResults(lintResult) {
+                if (lintResult.errors.length > 0) {
+                    lintResult.errors.forEach(function(error) {
+                        handleError(commit, lintResult.filename, error);
+                        log.lintError(event.repo.name, commit.sha,
+                                         lintResult.filename, error);
+                    });
+                } else {
+                    log.success(event.repo.name, commit.sha, lintResult.filename);
+                }
+            });
+
+        };
+
+        github.languages(event.repo, function handleLanguage(languages) {
+            if('C++' in languages) {
+                checkRepo(handleCheck);
+            } else {
+                log.ignore(event.repo.name);
+            }
+        }, function() {
+            checkRepo(handleCheck);
+        });
+    };
+
+    ioClient.on('pushevent', handlePushEvent);
 });
 
-function handlePushEvent(event) {
-    function checkRepo(callback) {
-        event.payload.commits.forEach(function (commit) {
-            github.patch(event.repo, commit, function (patch) {
-                callback(commit, linter.checkPatch(patch));
-            });
-        });
-    };
 
-    function handleError(commit, filename, error) {
-        github.file(event.repo.name, commit.sha, filename, function(file) {
-            lastError = {
-                actor: event.actor,
-                repo: event.repo,
-                commit: commit,
-                error: error,
-                filename: filename,
-                code: code.fragment(error.linenumber, file)
-            };
-            serverSocket.emit('linterror', lastError);
-        });
-    };
+ioClient.on('connect', function() {
+    console.log('Connected to ' + options.url);
+});
 
-    function handleCheck(commit, lintResults) {
-        if (lintResults.length === 0) {
-            log.skip(event.repo.name, commit.sha);
-        }
-
-        lintResults.forEach(function processLintResults(lintResult) {
-            if (lintResult.errors.length > 0) {
-                lintResult.errors.forEach(function(error) {
-                    handleError(commit, lintResult.filename, error);
-                    log.lintError(event.repo.name, commit.sha,
-                                     lintResult.filename, error);
-                });
-            } else {
-                log.success(event.repo.name, commit.sha, lintResult.filename);
-            }
-        });
-
-    };
-
-    github.languages(event.repo, function handleLanguage(languages) {
-        if('C++' in languages) {
-            checkRepo(handleCheck);
-        } else {
-            log.ignore(event.repo.name);
-        }
-    }, function() {
-        checkRepo(handleCheck);
-    });
-};
-
-app.use(express.static(path.join(__dirname, '/public')));
-http.listen(options.port);
-clientSocket.on('pushevent', handlePushEvent);
-serverSocket.on('connection', function(browserSocket) {
-    if(lastError) {
-        browserSocket.emit('linterror', lastError);
-    }
+ioClient.on('connect_error', function(err) {
+    console.error(err);
 });
